f4ab77b4462d6fb5c3b62159da4782a43ebdcf67
GS-Mz <gonzza.dev@gmail.com>
2022-10-25 23:19:02 -0300

    homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,27 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  let resultadoFin = 0;
+  let arrayNum = num.split("");
+  arrayNum.reverse();
+  for (let index = 0; index < arrayNum.length; index++) {
+    let resultadoProv = arrayNum[index] * (Math.pow(2, index));
+    resultadoFin = resultadoFin + resultadoProv;
+  }
+  return resultadoFin
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let binaryArray = [];
+  let divResult = 0;
+  do {
+    divResult = num % 2;
+    num = Math.floor(num / 2);
+    binaryArray.push(divResult);
+  } while (num >= 1);
+  binaryArray.reverse();
+  let binary = binaryArray.join("");
+  return binary
 }
 
 
