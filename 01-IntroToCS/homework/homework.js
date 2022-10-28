d7fae2b29f8155d658ed940f91da07e8d942dd0a
GS-Mz <gonzza.dev@gmail.com>
2022-10-27 22:05:53 -0300

    Correcciones

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,24 +2,14 @@
 
 function BinarioADecimal(num) {
   let resultadoFin = 0;
-  let arrayNum = num.split("");
-  arrayNum.reverse();
-  for (let index = 0; index < arrayNum.length; index++) {
-    let resultadoProv = arrayNum[index] * (Math.pow(2, index));
-    resultadoFin += resultadoProv;
+  let arrayNum = num.split("").reverse();;
+  for (let i = 0; i < arrayNum.length; i++) {
+    resultadoFin += arrayNum[i] * (Math.pow(2, i));
   }
   return resultadoFin
 }
 
-// function BinarioADecimal(num) {
-//   // tu codigo aca
-//   //parseInt( "11110",2 )**
-//   var result = 0;
-//   for (let i = num.length - 1; i >= 0; i--) {
-//     result =+ num.charAt(i) * Math.pow(2, i);
-//     //console.log(Math.pow(num[i] * 2 , i))
-//   } return result;
-// }
+
 
 // function DecimalABinario(num) {
 //   let binaryArray = [];
@@ -34,13 +24,22 @@ function BinarioADecimal(num) {
 //   return binary
 // }
 
+// function DecimalABinario(num) {
+//   let binaryResult = "";
+//   while (num > 0){
+//     binaryResult += num % 2;
+//     num = Math.floor(num / 2);
+//   }
+//   return binaryResult.split("").reverse().join("");
+// }
+
 function DecimalABinario(num) {
   let binaryResult = "";
   while (num > 0){
-    binaryResult += num % 2;
+    binaryResult = num % 2 + binaryResult;
     num = Math.floor(num / 2);
   }
-  return binaryResult.split("").reverse().join("");
+  return binaryResult
 }
 
 
