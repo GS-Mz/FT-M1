f2bddbb2a4091fa9d7e311dd95aa1086da8fa0bf
GS-Mz <gonzalosebastian20002@gmail.com>
2022-11-16 13:13:35 -0300

    arboles binarios inicio

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,78 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+    this.data = value;
+    this.left = null;
+    this.rigth = null;
+}
+
+BinarySearchTree.prototype.size = function (){
+    return
+}
+
+BinarySearchTree.prototype.insert = function(value){
+    //Si entra un array de numeros
+    if (typeof value == "object"){
+      value.forEach(number => {
+      this.insert(number);
+      });
+    }
+    // Declaración variables
+    let newThree = new BinarySearchTree(value)
+    let Lsuccess = "Nodo agregado con éxito a la izquierda"
+    let Rsuccess = "Nodo agregado con éxito a la derecha"
+
+    // Si hay nodos en el lugar
+    if (value < this.data && this.left != null){
+      return this.left.insert(value)
+    } else if (value > this.data && this.rigth != null){
+      return this.rigth.insert(value)
+    }
+    // Si no hay nodos en el lugar
+    if (value < this.data && !this.left){
+      this.left = newThree;
+      return Lsuccess
+    } else if (value > this.data && !this.rigth) {
+      this.rigth = newThree;
+      return Rsuccess
+    }
+}
+
+BinarySearchTree.prototype.contains = function(value){
+  //Si el nodo actual es el buscado
+  if (this.data === value) return true
+  //Si el valor es menor al del nodo actual
+  //Ejecuta el método en el nodo de la izquierda
+  if (value <= this.data && this.left !== null){
+    return this.left.contains(value)
+  }
+  //Si el valor es mayor al del nodo actual
+  //Ejecuta el metodo en el nodo de la derecha 
+  else if(value > this.data && this.rigth !== null){
+    return this.rigth.contains(value)
+  }
+  else return false
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function (type, output=[]){
+  if(type == "post-order") {
+      if (this.left === null && this.rigth === null){
+        output.push(this.data);
+       }
+      else if(this.left != null){
+        console.log(output)
+        return this.left.depthFirstForEach("post-order", output)
+    } 
+  
+  }  
+}
+
+const binaryTree = new BinarySearchTree(20);
+console.log(binaryTree.insert([15, 25, 5, 17, 21, 28, 0, 14, 50, 1, 45, 13, 12, 11, 30, 35, 33, 31, 34]))
+console.log(binaryTree);
+console.log(binaryTree.contains(2))
+console.log(binaryTree.depthFirstForEach("post-order"));
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
