2a183ab0cbc9180c18381112f367b32b4ce66eca
GS-Mz <gonzza.dev@gmail.com>
2022-11-21 22:44:27 -0300

    update

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,23 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let factor = [1];
+  let div = 2;
+  while (num > 1){
+    if (num % div === 0){
+      factor.push(div)
+      num /= div;
+    }else{
+      div++
+    }
+  }
+  return factor
+}
 
+function inOrderAsc(array){ //valida si el array pasado está ordenado ascendentemente
+  return array.every(function(_, i){ //.every es un método que valida que cada valor del array cumpla true en el callback | "_" es el nombre de una funcion sin relevancia
+    return i == 0 || array[i] >= array[i-1]; //retorna true en el callback si
+  });
 }
 
 function bubbleSort(array) {
@@ -14,7 +30,16 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  while(!inOrderAsc(array)){
+    for (let i = 0; i < array.length; i++) {
+      if(array[i] > array[i+1]){
+        let value = array[i];
+        array[i] = array[i+1];
+        array[i+1] = value;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +48,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i-1;
+    let aux = array[i];
+    while(j >= 0 && array[j] > aux){
+      array[j+1] = array[j];
+      j--
+    }
+    array[j+1] = aux;
+  }
+  return array
 }
 
 
@@ -35,6 +69,7 @@ function selectionSort(array) {
 
 }
 
+console.log(bubbleSort([10, -2, -7, 4, 4, 4, 5, 65, 2, 35, 14,15,23,18,2,15,18,94]))
 
 // No modificar nada debajo de esta línea
 // --------------------------------
